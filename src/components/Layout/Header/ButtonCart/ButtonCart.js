21fbe6e6d1eae9f821d6e480cc399a479c4219fc
vulebaolong <vulebaolong@gmail.com>
2023-03-12 21:03:44 +0700

    http get

--- src/components/Layout/Header/ButtonCart/ButtonCart.js
+++ src/components/Layout/Header/ButtonCart/ButtonCart.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 
 import style from "./ButtonCart.module.css";
 import CartIcon from "./IconCart";
@@ -6,9 +6,24 @@ import CartContext from "../../../store/cart-context.js";
 
 function ButtonCart(props) {
     const ctx = useContext(CartContext);
-    // console.log(ctx.items.length);
+    const { totalAmount } = ctx;
+    const refButton = useRef();
+
+    useEffect(() => {
+        const timming = setTimeout(function () {
+            // console.log("xóa");
+            refButton.current.classList.remove(`${style.bump}`);
+        }, 300);
+        // console.log("thêm");
+        refButton.current.classList.add(`${style.bump}`);
+
+        return function clear() {
+            // console.log("clear");
+            clearTimeout(timming);
+        };
+    }, [totalAmount]);
     return (
-        <button className={style.button} onClick={props.onShowCart}>
+        <button ref={refButton} className={style.button} onClick={props.onShowCart}>
             <span className={style.icon}>
                 <CartIcon />
             </span>
--- src/components/Meals/MealList/MealList.js
+++ src/components/Meals/MealList/MealList.js
@@ -1,50 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../../UI/Card/Card.js";
 import MealItem from "./MealItem/MealItem";
 
 import style from "./MealList.module.css";
 
-const DUMMY_MEALS = [
-    {
-        id: "m1",
-        name: "Sushi",
-        description: "Finest fish and veggies",
-        price: 22.99,
-    },
-    {
-        id: "m2",
-        name: "Schnitzel",
-        description: "A german specialty!",
-        price: 16.5,
-    },
-    {
-        id: "m3",
-        name: "Barbecue Burger",
-        description: "American, raw, meaty",
-        price: 12.99,
-    },
-    {
-        id: "m4",
-        name: "Green Bowl",
-        description: "Healthy...and green...",
-        price: 18.99,
-    },
-];
-
 function MealList(props) {
-    const mealList = DUMMY_MEALS.map((meal) => (
-        <MealItem
-            key={meal.id}
-            id={meal.id}
-            name={meal.name}
-            description={meal.description}
-            price={meal.price}
-        />
-    ));
+    const [dataGet, setDataGet] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [isFoundNo, setIsFoundNo] = useState(false);
+    const [isErorr, setisErorr] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch(
+                    "https://http-meals-reactjs-default-rtdb.firebaseio.com/meals.json"
+                );
+                if (!res.ok) {
+                    setIsLoading(false);
+                    setisErorr(true);
+                    return;
+                }
+                const data = await res.json();
+                if (!data) {
+                    setIsLoading(false);
+                    setIsFoundNo(true);
+                    return;
+                }
+
+                console.log(data);
+
+                const dataResult = [];
+                for (const key in data) {
+                    dataResult.push({
+                        id: key,
+                        name: data[key].name,
+                        description: data[key].description,
+                        price: data[key].price,
+                    });
+                }
+                setDataGet(dataResult);
+                setIsLoading(false);
+            } catch (error) {
+                setIsLoading(false);
+                setisErorr(true);
+                console.log(error);
+            }
+        };
+        fetchData();
+    }, []);
+
+    const mealList = () => {
+        console.log(dataGet.length);
+
+        if (isLoading) {
+            return <p className={style.loading}>Loading...</p>;
+        }
+        if (isFoundNo) {
+            return <p className={style.loading}>Found no</p>;
+        }
+        if (isErorr) {
+            return <p className={style.loading}>Error...</p>;
+        }
+        const mealList = dataGet.map((meal) => (
+            <MealItem
+                key={meal.id}
+                id={meal.id}
+                name={meal.name}
+                description={meal.description}
+                price={meal.price}
+            />
+        ));
+        return mealList;
+    };
+
     return (
         <section className={style.meal_list}>
             <Card>
-                <ul>{mealList}</ul>
+                <ul>{mealList()}</ul>
             </Card>
         </section>
     );
